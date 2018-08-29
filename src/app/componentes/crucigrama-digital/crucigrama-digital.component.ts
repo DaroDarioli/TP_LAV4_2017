8db18d4504c07d63ecf3c3ad324512395f87ad98
DaroDarioli <darioesteban21@hotmail.com>
2018-08-29 00:39:08 -0300

    solo componente crucigrama

---
+++ src/app/componentes/crucigrama-digital/crucigrama-digital.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crucigrama-digital',
+  templateUrl: './crucigrama-digital.component.html',
+  styleUrls: ['./crucigrama-digital.component.css']
+})
+export class CrucigramaDigitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
